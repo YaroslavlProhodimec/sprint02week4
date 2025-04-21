ad596268e41fc6e9b79f1ce44c41daab2b9ca406
Yaroslav <yaroslav.muratov.97@inbox.ru>
2025-04-21 19:55:24 +0300

    Fix files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import {runDB} from "./db";
+import { MongoClient } from "mongodb";
 import { app } from "./settings";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
+// Обработка favicon
+app.get('/favicon.ico', (req, res) => {
+    res.status(204).end();
+});
+
+app.get('/favicon.png', (req, res) => {
+    res.status(204).end();
+});
+
 app.get('/', (req, res) => {
-    res.send('API is working!');
     res.status(200).send('API is working!');
 });
+
 app.use((req, res) => {
     res.status(404).send('Not Found');
 });
+
 const startApp = async () => {
     try {
         await runDB();
